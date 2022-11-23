d7d5f7fba2cde3fd95f912cc7a76743159dbced5
OperatingSystemCMD <109055470+OperatingSystemCMD@users.noreply.github.com>
2022-11-23 18:19:28 -0500

    Developer Test of CMDOS 1.1
    We're making a Developer Test of CMDOS 1.1, this is the First Build.

---
+++ DeveloperTestVersion/Version 1-1/Build221123546.bat
@@ -0,0 +1,215 @@
+@echo off
+title Loading...
+echo Loading...
+ping localhost-n 2 > nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&#BGPP55YYYJJJJYYY55PPGB#&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BG5YJJ????????????????????????JJY5GB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@&BPYJ??????JJJYYY55555555555555YYYJJJ??????JYPB&@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@#PYJ????JJYY55PPPPPPPPPPPPPPPPPPPPPPPPPP55YYJJ????JYP#@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@#PJ????JYY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YYJ????JP#@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@#PJ????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????JP#@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@B5????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????5B@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@BY????Y5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5Y????YB@@@@@@@@@@@@@
+echo @@@@@@@@@@@#Y???JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ???Y#@@@@@@@@@@@
+echo @@@@@@@@@&5????Y5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY????5&@@@@@@@@@
+echo @@@@@@@@BJ???J5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5J???JB@@@@@@@@
+echo @@@@@@@P???J5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5J???P@@@@@@@
+echo @@@@@&5???J5PPPPPPPPPPPPPP55YY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YY55PPPPPPPPPPPPPP5J???5&@@@@@
+echo @@@@&Y???JPPPPPPPPPPPPPP?~::.::^75PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP57^::.::~?PPPPPPPPPPPPPPJ???Y&@@@@
+echo @@@@Y???YPPPPPPPPPPPPP5^.........:JPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPJ:.........^5PPPPPPPPPPPPPY???Y&@@@
+echo @@@5???JPPPPPPPPPPPPPP7...........^PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP^...........7PPPPPPPPPPPPPPJ???5@@@
+echo @@G???JPPPPPPPPPPPPPPP?...........~PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP~...........?PPPPPPPPPPPPPPPJ???G@@
+echo @&J???5PPPPPPPPPPPPPPPP?:.......:!5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5!:.......:?PPPPPPPPPPPPPPPP5???J&@
+echo @P???JPPPPPPPPPPPPPPPPPP5J7~~~!?YPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY?!~~~7J5PPPPPPPPPPPPPPPPPPJ???P@
+echo &J???5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5???J&
+echo B???JPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPJ???B
+echo P???YPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY???P
+echo 5???YPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY???5
+echo Y???YPPPPPPPPPPP57777777777777777777777777777777777777777777777777777777777777777775PPPPPPPPPPPY???Y
+echo 5???YPPPPPPPPPPPY..................................................................YPPPPPPPPPPPY???5
+echo P???YPPPPPPPPPPPP^................................................................^PPPPPPPPPPPPY???P
+echo B???JPPPPPPPPPPPP7................................................................7PPPPPPPPPPPPJ???B
+echo &J???5PPPPPPPPPPP5:..............................................................:5PPPPPPPPPPP5???J&
+echo @P???JPPPPPPPPPPPP?..............................................................?PPPPPPPPPPPPJ???P@
+echo @&J???5PPPPPPPPPPPP!............................................................!PPPPPPPPPPPP5???J&@
+echo @@G???JPPPPPPPPPPPPP!..........................................................!PPPPPPPPPPPPPJ???G@@
+echo @@@5???JPPPPPPPPPPPPP7........................................................7PPPPPPPPPPPPPJ???5@@@
+echo @@@&Y???YPPPPPPPPPPPPPJ^....................................................^JPPPPPPPPPPPPPY???Y&@@@
+echo @@@@&Y???YPPPPPPPPPPPPP57:................................................:75PPPPPPPPPPPPPY???Y&@@@@
+echo @@@@@&Y???J5PPPPPPPPPPPPP57:............................................:75PPPPPPPPPPPPP5J???Y&@@@@@
+echo @@@@@@@5???J5PPPPPPPPPPPPPP5?~........................................~?5PPPPPPPPPPPPPP5J???5@@@@@@@
+echo @@@@@@@@GJ???Y5PPPPPPPPPPPPPPPY7~:................................:~7YPPPPPPPPPPPPPPP5Y???JG@@@@@@@@
+echo @@@@@@@@@&5???JYPPPPPPPPPPPPPPPPP5J7~:........................:~7J5PPPPPPPPPPPPPPPPPY????5&@@@@@@@@@
+echo @@@@@@@@@@@BY???JYPPPPPPPPPPPPPPPPPPP5YJ7!~^^::::::::::^^~!?JY5PPPPPPPPPPPPPPPPPPPYJ???YB@@@@@@@@@@@
+echo @@@@@@@@@@@@@BY???JY5PPPPPPPPPPPPPPPPPPPPPPPP5555555555PPPPPPPPPPPPPPPPPPPPPPPP5YJ???YB@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@BY????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????YB@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@#PJ????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????JP#@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@&B5J????JY55PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP55YJ????J5B&@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@&BPY?????JJY55PPPPPPPPPPPPPPPPPPPPPPPPPPPP55YJJ?????YPB&@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@&BPYJ??????JJYYY5555555555555555YYYJJ??????JYPB&@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BP5YJ??????????????????????????JY5PB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGGP5YYYJJJJJJJJYYY5PGGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\
+ping localhost-n 2 > nul
+
+:Warning
+cls
+echo CMDOS
+echo.
+echo.
+echo.
+echo An Operating System in a Command Prompt
+ping localhost-n 2 > nul
+cls
+title Warning
+color 18
+echo CMD.O.S (Command Prompt Operating System)
+echo =========================================
+echo Warning: This Program is in Developer Test
+echo we test it for the second version.
+echo.
+echo.
+echo.
+echo Important this is a Idea Creation
+echo and it won't work in Real Life
+echo (S)tart the OS   (C)lose the OS
+set /p Warn=
+if %Warn% == s goto OS
+if %Warn% == c goto Exit
+goto Warning
+
+:OS
+title Setup
+color 3f
+cls
+ping localhost-n 2 > nul
+color 02
+echo WELCOME...
+pause
+echo.
+echo.
+echo ===========================
+echo  Please Make your Username
+set /p User=
+echo Please Enter the Password
+set /p Password=
+echo ===========================
+echo.
+echo Ok %User%. Please Wait...
+ping localhost-n 2 > nul
+cls
+
+:Login
+cls
+echo Login
+echo =============
+echo.
+echo Username: %User%
+echo.
+set /p Login=Password: 
+if %Login% == %Password% goto LoginSuccessful
+goto Login
+
+:LoginSuccessful
+echo Welcome %User%...
+echo.
+echo.
+echo.
+pause
+
+:CMDOS
+title CMDOS
+mode 48,30
+Batbox /h 0
+color 3f
+echo Username: %User%
+echo ==========================================
+echo.
+echo.
+echo The time is %time%
+echo.
+echo.
+echo.
+call Button 10 7 "CommandTube" 30 7 "Settings" 10 11 "Shutdown" 30 11 "Notepad" # Press
+GetInput /m %Press% /h 70
+
+if %errorlevel% == 1 goto CTube
+if %errorlevel% == 2 goto Settings
+if %errorlevel% == 3 goto Exit
+if %errorlevel% == 4 goto Notepad
+goto CMDOS
+
+:Notepad
+cls
+call Button 0 0 "Exit" # Press
+echo Welcome to Notepad, type "exitNotepad" to exit Notepad
+echo This isn't ready
+GetInput /m %Press% /h 70
+
+if %errorlevel% == 1 goto CMDOS
+goto Notepad
+
+:CTube
+cls
+ping localhost-n 2 > nul
+color fc
+echo.
+echo.
+echo  ______     ______     __    __     __    __     ______     __   __     _____     ______   __  __     ______     ______    
+echo /\  ___\   /\  __ \   /\ "-./  \   /\ "-./  \   /\  __ \   /\ "-.\ \   /\  __-.  /\__  _\ /\ \/\ \   /\  == \   /\  ___\   
+echo \ \ \____  \ \ \/\ \  \ \ \-./\ \  \ \ \-./\ \  \ \  __ \  \ \ \-.  \  \ \ \/\ \ \/_/\ \/ \ \ \_\ \  \ \  __<   \ \  __\   
+echo  \ \_____\  \ \_____\  \ \_\ \ \_\  \ \_\ \ \_\  \ \_\ \_\  \ \_\\"\_\  \ \____-    \ \_\  \ \_____\  \ \_____\  \ \_____\ 
+echo   \/_____/   \/_____/   \/_/  \/_/   \/_/  \/_/   \/_/\/_/   \/_/ \/_/   \/____/     \/_/   \/_____/   \/_____/   \/_____/ 
+ping localhost-n 2 > nul
+cls
+echo ERROR: This is not ready.
+ping localhost-n 2 > nul
+cls
+goto CMDOS
+
+:Settings
+cls
+echo Settings
+echo ==================================
+echo.
+echo 1) Change Background
+echo.
+echo 2) Change Button
+echo.
+echo 3) Close
+set /p picksettings=
+if %picksettings% == 1 goto Background
+if %picksettings% == 2 goto ChgButton
+if %picksettings% == 3 goto CMDOS
+
+:Background
+cls
+echo Please Wait...
+pause
+goto Settings
+
+:ChgButton
+cls
+echo Change Button
+echo ===========================
+echo Would you like to change the button
+call Button 10 4 "Yes" 20 4 "No" # Press
+GetInput /m %Press% /h /0
+
+if %errorlevel% == 1 goto ButtonSetup
+if %errorlevel% == 2 goto Settings
+goto ChgButton
+ 
+:ButtonSetup
+cls
+echo Loading...
+ping localhost-n 2 > nul
+echo.
+echo ERROR: This function isn't ready.
+ping localhost-n 2 > nul
+goto Settings
+ 
+:Exit
+cls
+color 06
+echo It is now safe to shutdown the PCX.
+pause
