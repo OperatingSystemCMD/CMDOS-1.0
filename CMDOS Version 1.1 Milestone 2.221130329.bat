a193a845ef6bf9e54fd430af387353fa33b9f4be
OperatingSystemCMD <109055470+OperatingSystemCMD@users.noreply.github.com>
2022-11-30 20:40:18 -0500

    Milestone 2 Build of CMDOS 1.1
    This is a Milestone 2 build, not a Milestone 4 Build

---
+++ CMDOS Version 1.1 Milestone 2.221130329.bat
@@ -0,0 +1,339 @@
+@echo off
+title Loading...
+echo Loading...
+set money=0
+ping localhost-n 2 > nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&#BGPP55YYYJJJJYYY55PPGB#&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BG5YJJ????????????????????????JJY5GB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@&BPYJ??????JJJYYY55555555555555YYYJJJ??????JYPB&@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@#PYJ????JJYY55PPPPPPPPPPPPPPPPPPPPPPPPPP55YYJJ????JYP#@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@#PJ????JYY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YYJ????JP#@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@#PJ????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????JP#@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@B5????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????5B@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@BY????Y5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5Y????YB@@@@@@@@@@@@@
+echo @@@@@@@@@@@#Y???JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ???Y#@@@@@@@@@@@
+echo @@@@@@@@@&5????Y5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY????5&@@@@@@@@@
+echo @@@@@@@@BJ???J5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5J???JB@@@@@@@@
+echo @@@@@@@P???J5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5J???P@@@@@@@
+echo @@@@@&5???J5PPPPPPPPPPPPPP55YY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YY55PPPPPPPPPPPPPP5J???5&@@@@@
+echo @@@@&Y???JPPPPPPPPPPPPPP?~::.::^75PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP57^::.::~?PPPPPPPPPPPPPPJ???Y&@@@@
+echo @@@@Y???YPPPPPPPPPPPPP5^.........:JPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPJ:.........^5PPPPPPPPPPPPPY???Y&@@@
+echo @@@5???JPPPPPPPPPPPPPP7...........^PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP^...........7PPPPPPPPPPPPPPJ???5@@@
+echo @@G???JPPPPPPPPPPPPPPP?...........~PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP~...........?PPPPPPPPPPPPPPPJ???G@@
+echo @&J???5PPPPPPPPPPPPPPPP?:.......:!5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5!:.......:?PPPPPPPPPPPPPPPP5???J&@
+echo @P???JPPPPPPPPPPPPPPPPPP5J7~~~!?YPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY?!~~~7J5PPPPPPPPPPPPPPPPPPJ???P@
+echo &J???5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5???J&
+echo B???JPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPJ???B
+echo P???YPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY???P
+echo 5???YPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPY???5
+echo Y???YPPPPPPPPPPP57777777777777777777777777777777777777777777777777777777777777777775PPPPPPPPPPPY???Y
+echo 5???YPPPPPPPPPPPY..................................................................YPPPPPPPPPPPY???5
+echo P???YPPPPPPPPPPPP^................................................................^PPPPPPPPPPPPY???P
+echo B???JPPPPPPPPPPPP7................................................................7PPPPPPPPPPPPJ???B
+echo &J???5PPPPPPPPPPP5:..............................................................:5PPPPPPPPPPP5???J&
+echo @P???JPPPPPPPPPPPP?..............................................................?PPPPPPPPPPPPJ???P@
+echo @&J???5PPPPPPPPPPPP!............................................................!PPPPPPPPPPPP5???J&@
+echo @@G???JPPPPPPPPPPPPP!..........................................................!PPPPPPPPPPPPPJ???G@@
+echo @@@5???JPPPPPPPPPPPPP7........................................................7PPPPPPPPPPPPPJ???5@@@
+echo @@@&Y???YPPPPPPPPPPPPPJ^....................................................^JPPPPPPPPPPPPPY???Y&@@@
+echo @@@@&Y???YPPPPPPPPPPPPP57:................................................:75PPPPPPPPPPPPPY???Y&@@@@
+echo @@@@@&Y???J5PPPPPPPPPPPPP57:............................................:75PPPPPPPPPPPPP5J???Y&@@@@@
+echo @@@@@@@5???J5PPPPPPPPPPPPPP5?~........................................~?5PPPPPPPPPPPPPP5J???5@@@@@@@
+echo @@@@@@@@GJ???Y5PPPPPPPPPPPPPPPY7~:................................:~7YPPPPPPPPPPPPPPP5Y???JG@@@@@@@@
+echo @@@@@@@@@&5???JYPPPPPPPPPPPPPPPPP5J7~:........................:~7J5PPPPPPPPPPPPPPPPPY????5&@@@@@@@@@
+echo @@@@@@@@@@@BY???JYPPPPPPPPPPPPPPPPPPP5YJ7!~^^::::::::::^^~!?JY5PPPPPPPPPPPPPPPPPPPYJ???YB@@@@@@@@@@@
+echo @@@@@@@@@@@@@BY???JY5PPPPPPPPPPPPPPPPPPPPPPPP5555555555PPPPPPPPPPPPPPPPPPPPPPPP5YJ???YB@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@BY????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????YB@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@#PJ????JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5YJ????JP#@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@&B5J????JY55PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP55YJ????J5B&@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@&BPY?????JJY55PPPPPPPPPPPPPPPPPPPPPPPPPPPP55YJJ?????YPB&@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@&BPYJ??????JJYYY5555555555555555YYYJJ??????JYPB&@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BP5YJ??????????????????????????JY5PB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGGP5YYYJJJJJJJJYYY5PGGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\
+ping localhost-n 2 > nul
+
+:Warning
+cls
+echo CMDOS
+echo.
+echo.
+echo.
+echo An Operating System in a Command Prompt
+ping localhost-n 2 > nul
+cls
+title Warning
+color 18
+echo CMDOS 1.1 Milestone 2 Build
+echo =========================================
+echo Warning: This Program is in Developer Test
+echo we test it for the second version.
+echo.
+echo.
+echo.
+echo Important this is a Idea Creation
+echo and it won't work in Real Life
+echo (S)tart the OS   (C)lose the OS
+set /p Warn=
+if %Warn% == s goto Setup
+if %Warn% == c goto Exit
+goto Warning
+
+:Setup
+title Setup
+color 00
+cls
+ping localhost-n 2 > nul
+color 3f
+echo WELCOME...
+pause
+
+:Seriel
+cls
+echo.
+echo Please enter the Seriel Key:
+echo Example: XXXXXXXX
+echo.
+set /p serielkey=
+if %serielkey% == EfJBqiNn goto Verified1
+goto Seriel
+
+:Verified1
+echo You've entered a Valid CMDOS 1.1 Premium Product
+echo.
+pause
+cls
+echo ===========================
+echo  Please Make your Username
+set /p User=
+echo Please Enter the Password
+set /p Password=
+echo ===========================
+echo.
+echo Ok %User%. The Computer will now shutdown...
+ping localhost-n 2 > nul
+cls
+
+:BootUp
+cls
+color 07
+ping localhost-n 2 > nul
+echo Chose a Boot Option
+echo.
+echo 1) Fast Boot
+echo 2) Normal Boot
+echo 3) Safe Boot
+choice /c 123
+if %errorlevel% == 1 goto fastboot
+if %errorlevel% == 2 goto Login
+if %errorlevel% == 3 goto safeboot
+goto BootUp
+
+:fastboot
+echo Loading Files...
+timeout 2 >nul
+echo Loading boot.bat...
+timeout 4 >nul
+echo Loading Desktop...
+timeout 6 >nul
+goto LoginSuccessful
+
+:Login
+cls
+echo Login
+echo =============
+echo.
+echo Username: %User%
+echo.
+set /p Login=Password: 
+if %Login% == %Password% goto LoginSuccessful
+goto Login
+
+:safeboot
+echo Loading Files
+timeout 2 >nul
+echo Loading Safeguard
+timeout 4 >nul
+goto SafeCMDOS
+
+:LoginSuccessful
+echo Welcome %User%...
+echo.
+echo.
+echo.
+pause
+
+:CMDOS
+title CMDOS
+mode 150,30
+Batbox /h 0
+color 3f
+echo Username: %User%                                                                                                           Money Earned: %money%
+echo =====================================================================================================================================================
+echo.
+echo.
+echo The time is %time%
+echo.
+echo.
+echo.
+call Button 10 7 "Menu" 30 7 "Settings" 10 11 "Shutdown" 30 11 "Notepad" 0 3 "Shop" # Press
+GetInput /m %Press% /h 70
+
+if %errorlevel% == 1 goto Menu
+if %errorlevel% == 2 goto Settings
+if %errorlevel% == 3 goto Exit
+if %errorlevel% == 4 goto Notepad
+if %errorlevel% == 5 goto Shop
+goto CMDOS
+
+:Shop
+cls
+echo Shop
+echo ======================================================
+echo  Coming Soon
+ping localhost-n 2 > nul
+goto CMDOS
+
+:SafeCMDOS
+cls
+echo Welcome to CMDOS Safe Boot!
+echo Please Choose these options:
+echo 1) Reboot 2) Shutdown
+choice /c 12
+if %errorlevel% == 1 goto CMDOS
+if %errorlevel% == 2 goto Exit
+
+:Menu
+cls
+echo Menu
+call Button 0 3 "Desktop" 0 6 "Internet" 0 9 "CMTube" # Press
+GetInput /m %Press% /h 70
+
+if %errorlevel% == 1 goto CMDOS
+if %errorlevel% == 2 goto StartGoogle
+if %errorlevel% == 3 goto CTube
+goto Menu
+
+:Notepad
+cls
+call Button 0 0 "Exit" # Press
+echo Welcome to Notepad, type "exitNotepad" to exit Notepad
+echo This isn't ready
+GetInput /m %Press% /h 70
+
+if %errorlevel% == 1 goto CMDOS
+goto Notepad
+
+:CTube
+cls
+color 0a
+echo Loading CTube...
+
+:Youtube
+cls
+ping localhost-n 2 > nul
+color fc
+echo.
+echo.
+echo  ______     ______     __    __     __    __     ______     __   __     _____     ______   __  __     ______     ______    
+echo /\  ___\   /\  __ \   /\ "-./  \   /\ "-./  \   /\  __ \   /\ "-.\ \   /\  __-.  /\__  _\ /\ \/\ \   /\  == \   /\  ___\   
+echo \ \ \____  \ \ \/\ \  \ \ \-./\ \  \ \ \-./\ \  \ \  __ \  \ \ \-.  \  \ \ \/\ \ \/_/\ \/ \ \ \_\ \  \ \  __<   \ \  __\   
+echo  \ \_____\  \ \_____\  \ \_\ \ \_\  \ \_\ \ \_\  \ \_\ \_\  \ \_\\"\_\  \ \____-    \ \_\  \ \_____\  \ \_____\  \ \_____\ 
+echo   \/_____/   \/_____/   \/_/  \/_/   \/_/  \/_/   \/_/\/_/   \/_/ \/_/   \/____/     \/_/   \/_____/   \/_____/   \/_____/ 
+ping localhost-n 2 > nul
+echo.
+set /p search=Search:  
+
+:ResultYT
+echo Searching...
+start https://www.youtube.com/results?search_query=%search%
+set /a money=%money% + 500
+call Button 30 20 "Exit" 0 6 "Continue" # Press
+GetInput /m %Press% /h 70
+
+if %errorlevel% == 1 goto CMDOS
+if %errorlevel% == 2 goto Youtube
+goto ResultYT
+
+:StartGoogle
+color 0a
+echo Loading Internet...
+
+:Google
+ping localhost-n 2 > nul
+echo @@@@@@@@JJJJJJJJJJ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@JJJJJJJJJJJJJJJJ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@JJJJ@@@@@@@@@@@@@@@@@
+echo @@@JJJJJ@@@@@@@@@@JJ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@JJJJ@@@@@@@@@@@@@@@@@
+echo @JJJJJ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@JJJJ@@@@@@@@@@@@@@@@@
+echo @JJJ&@@@@@@@@@@@@@@@@@@@@@@@@@@@YYYYY@@@@@@@@@@@@@~~~~@@@@@@@@@@@@@JJJJ@@@@@@@@JJJJ@@@@@@@YYYY@@@@@@
+echo JJJJ@@@@@@@@@@@@@@@@@@@@@@@@YYYYYYYYYYYY@@@@@@~~~~~~~~~~~~@@@@@@JJJJJJJJJJJJJ@@JJJJ@@@YYYYYYYYYYY@@@
+echo JJJJ@@@@@@@@@JJJJJJJJJJJJ@@YYYY@@@@@@@YYYY@@@~~~~@@@@@@~~~~@@@JJJJ@@@@@@@JJJJ@@JJJJ@@YYYY@@@@@@YYYY@
+echo &JJJ@@@@@@@@@@@@@@@@JJJJ@YYYY@@@@@@@@@YYY@@~~~~@@@@@@@@~~~~@@JJJ@@@@@@@@@JJJ@@JJJJ@YYYY@@@YYYYYYY@@
+echo @JJJJ@@@@@@@@@@@@@@@JJJJ@@YYYY@@@@@@@@@YYYY@~~~@@@@@@@@@@~~~@BJJJ@@@@@@@@@JJJ@@JJJJ@YYYYYYYYY@@@@@@@
+echo @@JJJJJ@@@@@@@@@@@@JJJJ@@@@YYYY@@@@@@@YYYY@@~~~~@@@@@@@@~~~~@@JJJJ@@@@@@@JJJJ@@JJJJ@@YYY5@@@@@@@@@@@
+echo @@@@JJJJJJJJJJJJJJJJJJ@@@@@@YYYYY#@YYYYYY@@@@~~~~~~@@~~~~~~@@@@JJJJJJ@YJJJJJJ@@JJJJ@@@YYYYY@@YYYYYY@
+echo @@@@@@@@JJJJJJJJJJJ@@@@@@@@@@@YYYYYYYYY@@@@@@@@@~~~~~~~~@@@@@@@@@JJJJJJJJ@JJJ@@JJJJ@@@@@YYYYYYYYY@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@JJJ@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@JJJJJ@@@@@JJJJ@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@JJJJJJJJJJJ@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&J@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo.
+set /p search=Search: 
+
+:Result
+echo Searching...
+start https://www.google.com/search?q=%search%
+set /a money=%money% + 500
+call Button 30 20 "Exit" 0 6 "Continue" # Press
+GetInput /m %Press% /h 70
+
+if %errorlevel% == 1 goto CMDOS
+if %errorlevel% == 2 goto Google
+goto Result
+
+:Settings
+cls
+echo Settings
+echo ==================================
+echo.
+echo 1) Change Background
+echo.
+echo 2) Change Button
+echo.
+echo 3) Close
+set /p picksettings=
+if %picksettings% == 1 goto Background
+if %picksettings% == 2 goto ChgButton
+if %picksettings% == 3 goto CMDOS
+
+:Background
+cls
+echo Please Wait...
+pause
+goto Settings
+
+:ChgButton
+cls
+echo Change Button
+echo ===========================
+echo Would you like to change the button
+call Button 10 4 "Yes" 20 4 "No" # Press
+GetInput /m %Press% /h /0
+
+if %errorlevel% == 1 goto ButtonSetup
+if %errorlevel% == 2 goto Settings
+goto ChgButton
+ 
+:ButtonSetup
+cls
+echo Loading...
+ping localhost-n 2 > nul
+echo.
+echo ERROR: This function isn't ready.
+ping localhost-n 2 > nul
+goto Settings
+ 
+:Exit
+cls
+color 06
+echo It is now safe to shutdown the PCX.
+pause
